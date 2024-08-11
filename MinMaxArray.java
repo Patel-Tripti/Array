d9e2b361a87e1b95e69d6463f55b74aae76b0de5
Patel-Tripti <139382407+Patel-Tripti@users.noreply.github.com>
2024-08-11 22:21:27 +0530

    Update MinMaxArray.java

--- MinMaxArray.java
+++ MinMaxArray.java
@@ -1,19 +1,12 @@
 class MinMaxArray {
     public static void main(String[] args) {
-        int arr[] = {5, 4, 7,8 ,10};
-        int temp =0;
-         
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-
-                    temp = arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-                }
-            }
-          System.out.print(arr[i]+" ");
+       int arr[] = {5, 4, 7,8 ,10};
+        int min = arr[0];
+        for(int i=1;i<arr.length;i++){
+         if(arr[i]<min){          //arr[i]>max for find the maximum value
+         min = arr[i];
+         }
         }
-        System.out.println("\nMinimum value is = "+arr[0]+"\nmaximum value is = "+arr[4]);
+         System.out.println("minimum value is = "+min);
     }
 }
